309e092c340f491c338a63cbb4fef32d0a72563e
Raja <maddula.rajarahul@gmail.com>
2018-12-09 23:13:19 +0530

    Action comments

--- src/app/common/util/reducerUtil.js
+++ src/app/common/util/reducerUtil.js
@@ -1,3 +1,4 @@
+//Allows us to reduce the boilerplate and switch in reducers
 export const createReducer = (initialState, fnMap) => {
   return (state = initialState, { type, payload }) => {
     const handler = fnMap[type];
