2615a966c4a115edbca2218130f4475372bb55a9
Raja <maddula.rajarahul@gmail.com>
2018-12-09 23:09:17 +0530

    Testing with Redux

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -32,6 +32,7 @@ class NavBar extends Component {
             School of AI
           </Menu.Item>
           <Menu.Item as={NavLink} to="/events" name="Meetups" />
+          <Menu.Item as={NavLink} to="/test" name="Test" />
           {authenticated && (
             <Menu.Item as={NavLink} to="/people" name="People" />
           )}
