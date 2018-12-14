1136f0c4301bab5fb51ec430856f1a1bde0217f3
Raja <maddula.rajarahul@gmail.com>
2018-12-14 23:44:46 +0530

    Configuring Firestore

---
+++ src/app/config/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4cghdsFv0qLJDFqzA9oPw_n2JR8Asb5g",
+  authDomain: "aievents.firebaseapp.com",
+  databaseURL: "https://aievents.firebaseio.com",
+  projectId: "aievents",
+  storageBucket: "",
+  messagingSenderId: "730949964762"
+};
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true
+};
+firestore.settings(settings);
+export default firebase;
